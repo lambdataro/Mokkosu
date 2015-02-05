5c555b17d9070bd166e7189fff1595d3a213d1ba
lambdataro <lambda.taro@gmail.com>
2015-02-06 02:09:38 +0900

    型スキームを作成

--- Mokkosu/MokkosuPrototype01/Type.cs
+++ Mokkosu/MokkosuPrototype01/Type.cs
@@ -1,4 +1,7 @@
-﻿namespace Mokkosu
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Mokkosu
 {
     /// <summary>
     /// 型の抽象クラス
@@ -57,6 +60,44 @@
         public override string ToString()
         {
             return string.Format("({0} -> {1})", ArgType, RetType);
+        }
+    }
+
+    /// <summary>
+    /// 型スキーム
+    /// </summary>
+    class TypeScheme
+    {
+        public ImmutableHashSet<int> Bounded { get; private set; }
+        public Type Type { get; private set; }
+
+        public TypeScheme(IEnumerable<int> bounded, Type type)
+        {
+            Bounded = new ImmutableHashSet<int>(bounded);
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            if (Bounded.Count == 0)
+            {
+                return Type.ToString();
+            }
+            else
+            {
+                return string.Format("∀{0}.{1}", BoundedToString(), Type);
+            }
+        }
+
+        string BoundedToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var id in Bounded.ToArray())
+            {
+                sb.Append(id);
+                sb.Append(", ");
+            }
+            return sb.ToString();
         }
     }
 }
---
+++ Mokkosu/MokkosuPrototype01/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Mokkosu
+{
+    class ImmutableHashSet<T>
+    {
+        HashSet<T> _hashset;
+
+        public ImmutableHashSet(IEnumerable<T> collection)
+        {
+            _hashset = new HashSet<T>(collection);
+        }
+
+        public bool Contains(T item)
+        {
+            return _hashset.Contains(item);
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _hashset.Count;
+            }
+        }
+
+        public T[] ToArray()
+        {
+            return _hashset.ToArray<T>();
+        }
+    }
+}
