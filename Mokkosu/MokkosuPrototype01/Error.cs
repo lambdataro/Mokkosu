9874fd244d0623544d9ed90ac7d89371fb418157
lambdataro <lambda.taro@gmail.com>
2015-02-05 20:01:32 +0900

    Errorクラスを実�

---
+++ Mokkosu/MokkosuPrototype01/Error.cs
@@ -0,0 +1,11 @@
+﻿﻿using System;
+
+namespace Mokkosu
+{
+    class Error : Exception
+    {
+        public Error() : base() { }
+        public Error(string message) : base(message) { }
+        public Error(string message, Exception inner) : base(message, inner) { }
+    }
+}
