02aefa565f17cf3ab5293317a9e5aae72e07ec38
lambdataro <lambda.taro@gmail.com>
2015-02-09 01:17:58 +0900

    SourceFileクラスを実�

---
+++ Mokkosu/MokkosuPrototype01/Input/SourceFile.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace Mokkosu.Input
+{
+    class SourceFile
+    {
+        string _name;
+        int _line;
+        TextReader _tr;
+
+        public SourceFile(TextReader tr, string name)
+        {
+            _name = name;
+            _line = 1;
+            _tr = tr;
+        }
+
+        public int Read()
+        {
+            var ch = _tr.Read();
+            if (ch == '\n')
+            {
+                _line++;
+            }
+            return ch;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}({1})", _name, _line);
+        }
+    }
+}
