b0cc0b767d6a1c690c4739321409f7e4a72b9916
lambdataro <lambda.taro@gmail.com>
2015-02-09 07:25:55 +0900

    MTypeを追加し基本的な型を追加。

---
+++ Mokkosu/MokkosuPrototype01/AST/MType.cs
@@ -0,0 +1,87 @@
+﻿namespace Mokkosu.AST
+{
+    abstract class MType
+    {
+    }
+
+    class TypeVar : MType
+    {
+        static int var_id_counter = 0;
+
+        public int Id { get; private set; }
+        public MType Value { get; set; }
+
+        public TypeVar()
+        {
+            Id = var_id_counter++;
+            Value = null;
+        }
+
+        public override string ToString()
+        {
+            if (Value == null)
+            {
+                return string.Format("<{0}>", Id);
+            }
+            else
+            {
+                return Value.ToString();
+            }
+        }
+    }
+
+    class UserType : MType
+    {
+        public string Name { get; private set; }
+
+        public UserType(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    class IntType : MType
+    {
+        public override string ToString()
+        {
+            return "Int";
+        }
+    }
+
+    class DoubleType : MType
+    {
+        public override string ToString()
+        {
+            return "Double";
+        }
+    }
+
+    class StringType : MType
+    {
+        public override string ToString()
+        {
+            return "String";
+        }
+    }
+
+    class CharType : MType
+    {
+        public override string ToString()
+        {
+            return "Char";
+        }
+    }
+
+    class UnitType : MType
+    {
+        public override string ToString()
+        {
+            return "()";
+        }
+    }
+}
