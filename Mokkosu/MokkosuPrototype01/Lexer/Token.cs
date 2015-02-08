092cc42a933e4ec1dcd32217ac3e6c21cac24d49
lambdataro <lambda.taro@gmail.com>
2015-02-09 01:12:45 +0900

    Tokenクラスを実�

---
+++ Mokkosu/MokkosuPrototype01/Lexer/Token.cs
@@ -0,0 +1,34 @@
+﻿namespace Mokkosu.Lexer
+{
+    enum TokenType
+    {
+        // 意味値を持つトークン
+        INT,
+        // 制御記号
+        EOF
+    }
+
+    class Token
+    {
+        public TokenType Type { get; private set; }
+        public int IntVal { get; private set; }
+        public string StrVal { get; private set; }
+
+        public Token(TokenType type)
+        {
+            Type = type;
+        }
+
+        public Token(TokenType type, int int_val)
+            : this(type)
+        {
+            IntVal = int_val;
+        }
+
+        public Token(TokenType type, string str_val)
+            : this(type)
+        {
+            StrVal = str_val;
+        }
+    }
+}
