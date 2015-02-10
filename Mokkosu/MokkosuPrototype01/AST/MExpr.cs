13cb2eba41c285aee6956dc53fe1f415becfdfc0
lambdataro <lambda.taro@gmail.com>
2015-02-10 20:49:14 +0900

    ラムダ式の抽象構文木を定義。

--- Mokkosu/MokkosuPrototype01/AST/MExpr.cs
+++ Mokkosu/MokkosuPrototype01/AST/MExpr.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections.Generic;
 namespace Mokkosu.AST
 {
+    /// <summary>
+    /// 式の抽象クラス
+    /// </summary>
     abstract class MExpr
     {
     }
 
+    /// <summary>
+    /// 整数定数
+    /// </summary>
     class MInt : MExpr
     {
         public int Value { get; private set; }
@@ -20,6 +26,9 @@ namespace Mokkosu.AST
         }
     }
 
+    /// <summary>
+    /// 浮動小数点数定数
+    /// </summary>
     class MDouble : MExpr
     {
         public double Value { get; private set; }
@@ -35,6 +44,9 @@ namespace Mokkosu.AST
         }
     }
 
+    /// <summary>
+    /// 文字列定数
+    /// </summary>
     class MString : MExpr
     {
         public string Value { get; private set; }
@@ -50,6 +62,9 @@ namespace Mokkosu.AST
         }
     }
 
+    /// <summary>
+    /// 文字定数
+    /// </summary>
     class MChar : MExpr
     {
         public char Value { get; private set; }
@@ -65,6 +80,9 @@ namespace Mokkosu.AST
         }
     }
 
+    /// <summary>
+    /// ユニット定数
+    /// </summary>
     class MUnit : MExpr
     {
         public override string ToString()
@@ -73,6 +91,9 @@ namespace Mokkosu.AST
         }
     }
 
+    /// <summary>
+    /// タグ
+    /// </summary>
     class MTag : MExpr
     {
         public string Name { get; private set; }
@@ -96,6 +117,75 @@ namespace Mokkosu.AST
             {
                 return string.Format("{0}({1})", Name, Utils.Utils.ListToString(Args));
             }
+        }
+    }
+
+    /// <summary>
+    /// 変数
+    /// </summary>
+    class MVar : MExpr
+    {
+        public string Name { get; private set; }
+        public MType Type { get; private set; }
+
+        public MVar(string name)
+        {
+            Name = name;
+            Type = new TypeVar();
+        }
+
+        public MVar(string name, MType type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0} : {1})", Name, Type);
+        }
+    }
+
+    class MLambda : MExpr
+    {
+        public string ArgName { get; private set; }
+        public MType ArgType { get; private set; }
+        public MExpr Body { get; private set; }
+
+        public MLambda(string arg_name, MExpr body)
+        {
+            ArgName = arg_name;
+            ArgType = new TypeVar();
+            Body = body;
+        }
+
+        public MLambda(string arg_name, MType arg_type, MExpr body)
+        {
+            ArgName = arg_name;
+            ArgType = arg_type;
+            Body = body;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("(\\{0} : {1} -> {2})", ArgName, ArgType, Body);
+        }
+    }
+
+    class MApp : MExpr
+    {
+        public MExpr FunExpr { get; private set; }
+        public MExpr ArgExpr { get; private set; }
+
+        public MApp(MExpr fun_expr, MExpr arg_expr)
+        {
+            FunExpr = fun_expr;
+            ArgExpr = arg_expr;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0} {1})", FunExpr, ArgExpr);
         }
     }
 }
