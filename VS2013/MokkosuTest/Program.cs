6d47b46058be24f811584e7092703e22a2a77cda
lambdataro <lambda.taro@gmail.com>
2015-02-16 05:52:16 +0900

    MokkosuTestとMokkosuPadを追加。

---
+++ VS2013/MokkosuTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace MokkosuTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mokkosu = new Mokkosu.Main.Mokkosu();
+
+            foreach (var fname in args)
+            {
+                mokkosu.AddSourceFile(fname);
+            }
+
+            if (args.Length != 0)
+            {
+                var name = Path.GetFileNameWithoutExtension(args.Last());
+                mokkosu.Compile(name);
+                mokkosu.Run();
+            }
+        }
+    }
+}
