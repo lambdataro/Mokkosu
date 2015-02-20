e9d337c4c00fafc2631d110541e7ec2e27e33c23
lambdataro <lambda.taro@gmail.com>
2015-02-20 19:14:46 +0900

    集約エラーハンドラを実�

--- VS2013/MokkosuPad/App.xaml.cs
+++ VS2013/MokkosuPad/App.xaml.cs
@@ -17,20 +17,21 @@ namespace MokkosuPad
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             DispatcherHelper.UIDispatcher = Dispatcher;
-            //AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
         }
 
         //集約エラーハンドラ
-        //private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-        //{
-        //    //TODO:ロギング処理など
-        //    MessageBox.Show(
-        //        "不明なエラーが発生しました。アプリケーションを終了します。",
-        //        "エラー",
-        //        MessageBoxButton.OK,
-        //        MessageBoxImage.Error);
-        //
-        //    Environment.Exit(1);
-        //}
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            //TODO:ロギング処理など
+            MessageBox.Show(
+                "不明なエラーが発生しました。アプリケーションを終了します。\n" +
+                e.ToString(),
+                "エラー",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+
+            Environment.Exit(1);
+        }
     }
 }
