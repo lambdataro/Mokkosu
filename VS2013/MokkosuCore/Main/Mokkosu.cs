fb33f8389e24bab91142ffa7ef421b7a6d2ea4a9
lambdataro <lambda.taro@gmail.com>
2015-03-06 02:48:40 +0900

    Version 1.1.3 に更新。

--- VS2013/MokkosuCore/Main/Mokkosu.cs
+++ VS2013/MokkosuCore/Main/Mokkosu.cs
@@ -37,7 +37,7 @@ namespace Mokkosu.Main
             sb.AppendLine(@" | |  | || (_) ||   < |   <| (_) |\__ \| |_| |");
             sb.AppendLine(@" |_|  |_| \___/ |_|\_\|_|\_\\___/ |___/ \__,_|");
             sb.AppendLine(@"");
-            sb.AppendLine(@" =============== Version 1.1.2 ===============");
+            sb.AppendLine(@" =============== Version 1.1.3 ===============");
             return sb.ToString();
         }
 
